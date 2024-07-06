193712e0f9e509b864ac88ec1b40b11ffe5f7172
VakuumimKopf <151374312+VakuumimKopf@users.noreply.github.com>
2024-07-06 02:07:33 +0200

    Hinzufügen von weiterem Window

--- DiaCreator/StartWindow.xaml.cs
+++ DiaCreator/StartWindow.xaml.cs
@@ -14,9 +14,9 @@ namespace DiaCreator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StartWindow : Window
     {
-        public MainWindow()
+        public StartWindow()
         {
             InitializeComponent();
         }
