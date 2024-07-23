8d55641a22d6c928ba0a068bc1c335e0cf97e86b
VakuumimKopf <151374312+VakuumimKopf@users.noreply.github.com>
2024-07-23 11:13:19 +0200

    Funktionalität des StartWindows in eigene ViewModell Klasse ausgelagert

--- DiaCreator/Windows/StartWindow.xaml.cs
+++ DiaCreator/Windows/StartWindow.xaml.cs
@@ -17,50 +17,13 @@ namespace DiaCreator
     /// </summary>
     public partial class StartWindow : Window
     {
-        private string? diagrammtyp;
-        private string? filepath;
         public StartWindow()
         {
             InitializeComponent();
-        }
-        private void ClickAddFile(object sender, RoutedEventArgs e)
-        {
-            // Einstellung der Dialogbox zum öffnen einer Datei
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.FileName = "Document"; // Default Datei Name
-            dialog.DefaultExt = ".csv"; // Default Dateityp
-            dialog.Filter = "Data documents (.csv)|*.csv"; // Filter für Dateityp
+            var vm = new StartWindowViewModell();
+            this.DataContext = vm;
+            vm.OnRequestClose += (sender, e) => this.Close();
 
-            // Anzeigen der Dialogbox
-            bool? result = dialog.ShowDialog();
-
-            // Speichern der geöffneten Datei als Pfad im Feld filepath
-            if (result == true)
-            {
-                filepath = dialog.FileName;
-            }
-        }
-        private void ClickErstelleNeuesProjekt(object sender, RoutedEventArgs e) 
-        {
-            // Ein Reader wird erzeugt passend zum Dateityp und in die golbale Property CurrentReader geschrieben, ihm wird der Pfad zur geöffneten Datei übergeben
-            if (filepath == null) { throw new Exception("Keine Datei übergeben"); }
-            App.CurrentReader = App.CurrentBuilder.CreateReader("csv", filepath);
-            // Der Reader ließt die ihm übergebene Datei aus und speichert den gelesenen Ihnhalt in den globalen DHolder
-            App.CurrentDHolder.AddDataList(App.CurrentReader.GetFileData());
-
-            // Das Fenster DataWindow wird erzeugt und das Startfenster geschlossen
-            if (diagrammtyp == null) { throw new Exception("Kein Diagrammtyp übergeben"); }
-            var DatenWindow = new DatenWindow();
-            DatenWindow.DataContext = new DatenWindowViewModell(diagrammtyp);
-            Close();
-            DatenWindow.Show();
-            
-        }
-
-        private void DiaSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            // Das ausgewählte Argument in der Combobox wird in dem Feld diagrammtyp gespeichert
-            diagrammtyp = ((ComboBoxItem)Test.SelectedItem).Content.ToString();   
         }
     }
 }
